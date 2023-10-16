7e742594ad522bcfd8acc30dbe65d981410e9d5a
Momchil Jeliazkov <jeliazkov.momchil@gmail.com>
2023-10-16 13:53:15 +0300

    initial

---
+++ src/main.cpp
@@ -0,0 +1,73 @@
+#include <chrono>
+#include <iostream>
+#include <random>
+
+#include "ggml.h"
+
+
+
+void print_tensor(struct ggml_tensor * tensor, const char* name) {
+    printf("%s:\n", name);
+    for (int i=0; i<tensor->ne[3]; i++) {
+        for (int j=0; j<tensor->ne[2]; j++) {
+            for (int k=0; k<tensor->ne[1]; k++) {
+                for (int l=0; l<tensor->ne[0]; l++) {
+                    if (tensor->ne[3] > 1) {
+                        printf("y[%d][%d][%d][%d] = %5.10f\n", l, k, j, i, ggml_get_f32_nd(tensor, l, k, j, i));
+                    } else {
+                        if (tensor->ne[2] > 1) {
+                            printf("y[%d][%d][%d] = %5.10f\n", l, k, j, ggml_get_f32_nd(tensor, l, k, j, i));
+                        } else {
+                            if (tensor->ne[1] > 1) {
+                                printf("y[%d][%d] = %5.10f\n", l, k, ggml_get_f32_nd(tensor, l, k, j, i));
+                            } else {
+                                printf("y[%d] = %5.10f\n", l, ggml_get_f32_nd(tensor, l, k, j, i));
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+}
+
+int main()
+{
+    struct ggml_init_params params = {
+        .mem_size = 16 * 1024 * 1024,
+        .mem_buffer = NULL,
+    };
+
+
+    // memory allocation happens here
+    struct ggml_context* ctx = ggml_init(params);
+
+    struct ggml_tensor* x = ggml_new_tensor_1d(ctx, GGML_TYPE_F32, 9);
+    ggml_set_f32_1d(x, 0, 0.0001f);
+    ggml_set_f32_1d(x, 1, 0.001f);
+    ggml_set_f32_1d(x, 2, 0.01f);
+    ggml_set_f32_1d(x, 3, 0.1f);
+    ggml_set_f32_1d(x, 4, 1.0f);
+    ggml_set_f32_1d(x, 5, 10.0f);
+    ggml_set_f32_1d(x, 6, 100.0f);
+    ggml_set_f32_1d(x, 7, 1000.0f);
+    ggml_set_f32_1d(x, 8, 10000.0f);
+
+
+
+    struct ggml_tensor* y = ggml_tanh(ctx, x);
+
+
+    struct ggml_cgraph gf = ggml_build_forward(y);
+
+
+    ggml_graph_compute_with_ctx(ctx, &gf, 4); 
+
+    print_tensor(y, "y");
+
+
+
+
+    return 0;
+}
