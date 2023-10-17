e897a7d9e7cb8fc09168e4810d155af2ea416008
Momchil Jeliazkov <jeliazkov.momchil@gmail.com>
2023-10-17 10:49:10 +0300

    some metal testing

--- src/main.cpp
+++ src/main.cpp
@@ -14,15 +14,15 @@ void print_tensor(struct ggml_tensor * tensor, const char* name) {
             for (int k=0; k<tensor->ne[1]; k++) {
                 for (int l=0; l<tensor->ne[0]; l++) {
                     if (tensor->ne[3] > 1) {
-                        printf("y[%d][%d][%d][%d] = %5.10f\n", l, k, j, i, ggml_get_f32_nd(tensor, l, k, j, i));
+                        printf("%s[%d][%d][%d][%d] = %5.10f\n", name, l, k, j, i, ggml_get_f32_nd(tensor, l, k, j, i));
                     } else {
                         if (tensor->ne[2] > 1) {
-                            printf("y[%d][%d][%d] = %5.10f\n", l, k, j, ggml_get_f32_nd(tensor, l, k, j, i));
+                            printf("%s[%d][%d][%d] = %5.10f\n", name, l, k, j, ggml_get_f32_nd(tensor, l, k, j, i));
                         } else {
                             if (tensor->ne[1] > 1) {
-                                printf("y[%d][%d] = %5.10f\n", l, k, ggml_get_f32_nd(tensor, l, k, j, i));
+                                printf("%s[%d][%d] = %5.10f\n", name, l, k, ggml_get_f32_nd(tensor, l, k, j, i));
                             } else {
-                                printf("y[%d] = %5.10f\n", l, ggml_get_f32_nd(tensor, l, k, j, i));
+                                printf("%s[%d] = %5.10f\n", name, l, ggml_get_f32_nd(tensor, l, k, j, i));
                             }
                         }
                     }
@@ -33,9 +33,68 @@ void print_tensor(struct ggml_tensor * tensor, const char* name) {
     
 }
 
+
+static void ggml_log_callback_default(ggml_log_level level, const char * text, void * user_data) {
+    (void) level;
+    (void) user_data;
+    fputs(text, stderr);
+    fflush(stderr);
+}
+
+
+#if MY_GGML_USE_METAL
+#include "ggml-metal.h"
+void runOnMetal()
+{
+    std::cout <<"Running on Metal" << std::endl;
+    ggml_metal_log_set_callback(ggml_log_callback_default, nullptr);
+
+//    ggml_backend_t backend = NULL;
+//    backend = ggml_backend_metal_init();
+//    if (backend) {
+//        ggml_backend_free(backend);
+//    }
+
+
+    struct ggml_init_params params = {
+            .mem_size = 16 * 1024 * 1024,
+            .mem_buffer = NULL,
+        };
+
+    struct ggml_context* ctx = ggml_init(params);
+    struct ggml_tensor* x = ggml_new_tensor_1d(ctx, GGML_TYPE_F32, 5);
+    ggml_set_f32_1d(x, 0, 0.01f);
+    ggml_set_f32_1d(x, 1, 0.1f);
+    ggml_set_f32_1d(x, 2, 1.0f);
+    ggml_set_f32_1d(x, 3, 10.0f);
+    ggml_set_f32_1d(x, 4, 100.0f);
+    struct ggml_tensor* y = ggml_silu(ctx, x);
+    struct ggml_cgraph gf = ggml_build_forward(y);
+
+
+    auto ctxm = ggml_metal_init(1);
+    ggml_metal_add_buffer(ctxm, "base", ggml_get_mem_buffer(ctx), ggml_get_mem_size(ctx), 0);
+
+    ggml_metal_set_tensor(ctxm, x);
+    ggml_metal_graph_compute(ctxm, &gf);
+    ggml_metal_get_tensor(ctxm, y);
+
+    print_tensor(y, "ym");
+
+
+
+    if (ctxm) ggml_metal_free(ctxm);
+    if (ctx) ggml_free(ctx);
+
+}
+#endif
+
+
 int main()
 {
-
+#if MY_GGML_USE_METAL
+    runOnMetal();
+#endif
 
 	// 1. We create a descriptor
 	WGPUInstanceDescriptor desc = {};
@@ -72,22 +131,16 @@ int main()
     // ggml_backend_t backend = NULL;
     // backend = ggml_backend_cpu_init();
 
-    struct ggml_tensor* x = ggml_new_tensor_1d(ctx, GGML_TYPE_F32, 9);
-
-
-    ggml_set_f32_1d(x, 0, 0.0001f);
-    ggml_set_f32_1d(x, 1, 0.001f);
-    ggml_set_f32_1d(x, 2, 0.01f);
-    ggml_set_f32_1d(x, 3, 0.1f);
-    ggml_set_f32_1d(x, 4, 1.0f);
-    ggml_set_f32_1d(x, 5, 10.0f);
-    ggml_set_f32_1d(x, 6, 100.0f);
-    ggml_set_f32_1d(x, 7, 1000.0f);
-    ggml_set_f32_1d(x, 8, 10000.0f);
+    struct ggml_tensor* x = ggml_new_tensor_1d(ctx, GGML_TYPE_F32, 5);
+    ggml_set_f32_1d(x, 0, 0.01f);
+    ggml_set_f32_1d(x, 1, 0.1f);
+    ggml_set_f32_1d(x, 2, 1.0f);
+    ggml_set_f32_1d(x, 3, 10.0f);
+    ggml_set_f32_1d(x, 4, 100.0f);
 
 
 
-    struct ggml_tensor* y = ggml_tanh(ctx, x);
+    struct ggml_tensor* y = ggml_silu(ctx, x);
 
 
     struct ggml_cgraph gf = ggml_build_forward(y);
